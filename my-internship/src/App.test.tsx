bb1921a1dc3b1a07826b509d99d2b05466bc6c7e
Kl4y4 <surowiec.bartosz@wp.pl>
2023-07-14 18:45:19 +0200

    Modified App code and updated test

--- my-internship/src/App.test.tsx
+++ my-internship/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appComponent = screen.getByText('Hello world!');
+  expect(appComponent).toBeInTheDocument();
 });
