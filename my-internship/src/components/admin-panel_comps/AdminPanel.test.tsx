76f1be54f5ff5f18fdbf7bae771f0268528d4d1d
Kl4y4 <surowiec.bartosz@wp.pl>
2023-07-18 16:11:15 +0200

    Updated test

--- my-internship/src/components/admin-panel_comps/AdminPanel.test.tsx
+++ my-internship/src/components/admin-panel_comps/AdminPanel.test.tsx
@@ -15,7 +15,7 @@ Object.defineProperty(window, 'matchMedia', {
 test('Closing form is rendered', () => {
   render(<AdminPanel isOpen>
     <form data-testid='close-dialog' className='close-dialog' method="dialog"><button type="submit">x</button></form>
-  </AdminPanel>);
+  </AdminPanel>)
   const closingForm = screen.getByTestId('close-dialog')
   expect(closingForm).toBeInTheDocument()
 });
