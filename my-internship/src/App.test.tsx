c4223c1b626041a1d40822320fe49b22ab60654f
Kl4y4 <surowiec.bartosz@wp.pl>
2023-07-18 15:14:07 +0200

    Updated tests

--- my-internship/src/App.test.tsx
+++ my-internship/src/App.test.tsx
@@ -1,9 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
+Object.defineProperty(window, 'matchMedia', {
+  value: () => {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {}
+    };
+  }
+})
+
 test('renders app component', () => {
-  render(<App />);
+  act(() => render(<App />))
+  
   const appComponent = screen.getByTestId("app")
   expect(appComponent).toBeInTheDocument();
 });
--- my-internship/src/components/ticket-app_pages/TicketApp.test.tsx
+++ my-internship/src/components/ticket-app_pages/TicketApp.test.tsx
@@ -1,9 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../../App'
 
+Object.defineProperty(window, 'matchMedia', {
+  value: () => {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {}
+    };
+  }
+})
+
 test('renders home page', () => {
   render(<App />);
+  const ticketAppBtn = screen.getAllByRole("button")[0]
+  act(() => fireEvent(ticketAppBtn, new Event("click")))
   const ticketAppComponent = screen.getByTestId('main')
   expect(ticketAppComponent).toHaveTextContent('Main');
 });
